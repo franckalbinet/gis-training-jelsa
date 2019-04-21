c20df98f1264f3e13b5f0990fbd6ccf1c7fd97af
root <root@ganesh.localdomain>
2019-04-21 17:46:05 +0200

    resize images

--- src/img/resize.sh
+++ src/img/resize.sh
@@ -4,7 +4,7 @@ cp ../img-origin/*.PNG .
 for i in $( ls *.PNG ); do
   filename=$(basename "$i")
   filename="${filename%.*}"
-  convert $i -resize 750x750 $i
+  convert $i -resize 600x600 $i
 done
 
 
