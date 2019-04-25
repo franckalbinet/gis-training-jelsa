d5c084776574b133d6285fcd2bf53f8319b26eba
root <root@ganesh.localdomain>
2019-04-25 11:47:37 +0200

    build

--- downloads/build.sh
+++ downloads/build.sh
@@ -1,1 +1,12 @@
-zip -r gis_ghana_course_animal_health.zip ../ghana-2016
+#!/bin/bash
+
+mkdir jelsa-gis
+
+cp -R ../data jelsa-gis
+cp -R ../pdf jelsa-gis
+cp -R ../ppt jelsa-gis
+cp -R ../resources jelsa-gis
+cp -R ../src jelsa-gis
+
+zip -r jelsa-gis.zip ../jelsa-gis
+rm -r jelsa-gis
